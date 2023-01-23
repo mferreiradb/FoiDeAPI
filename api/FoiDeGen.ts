15dac4c7c000ab551784f1a8d7a656472ec1bb15
Mauricio <mauhmg@gmail.com>
2023-01-23 15:21:15 -0300

    Inserção de novas frases'

--- api/FoiDeGen.ts
+++ api/FoiDeGen.ts
@@ -1,6 +1,26 @@
 const frases: string[] = [
     "Foi de base",
-    "Foi de arrasta pra cima"
+    "Foi de arrasta pra cima",
+    "Foide hexa 2022",
+    "Foi de comes e bebes",
+    "Foi de deitado eternamente em berço esplêndido",
+    "Foi de sevira nos 30",
+    "Foi de Wakanda Forever na horizontal",
+    "Foi de link na bio",
+    "Foi de camisa de saudades",
+    "Foi de foto em preto e branco",
+    "Foi de Jacksons Five",
+    "Foi de F no chat",
+    "Foi de Drake e Josh",
+    "Foi pra terra jogar de pé junto",
+    "Foi jogar no Vasco",
+    "Foi de Rainha Elizabeth",
+    "Foi de Encontro com Fátima Bernardes",
+    "Foi de Bom dia e Cia",
+    "Foi pro show do Avicii",
+    "Foi de lojas americanas",
+    "Foi de Pelé Nascimento Arantes",
+    "Foi de maradona"
 ]
 
 export function gerarFrase() {
