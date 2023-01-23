9dca71df2176f5bb5b4faeb057b3fbc3a2975934
mferreiradb <mauhmg@gmail.com>
2023-01-23 00:42:29 -0300

    Teste da API

--- api/FoiDeGen.ts
+++ api/FoiDeGen.ts
@@ -1,1 +1,11 @@
-const frases: string[] = ["Foi de base", "Foi de arrasta pra cima"]
+const frases: string[] = [
+    "Foi de base",
+    "Foi de arrasta pra cima"
+]
+
+export function gerarFrase() {
+    const i = Math.floor(Math.random() * frases.length)
+    const frase = frases[i]
+    console.log(frase)
+    return frase
+}
--- api/main.ts
+++ api/main.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import { gerarFrase } from './FoiDeGen'
 
 const app = express()
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
-app.listen(1010, () => console.log('Online em: http://localhost:1010'))
+app.get('/', (req, res) => {
+    const frase = gerarFrase()
+    return res.json(frase)
+})
+
+app.listen(3333, () => console.log('Online em http://localhost:3333'))
