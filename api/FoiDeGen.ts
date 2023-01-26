10c29c58675c9429bf9bcad20173dcd636ce2e0d
Mauricio <mauhmg@gmail.com>
2023-01-26 14:32:15 -0300

    Correção via eslint

--- api/FoiDeGen.ts
+++ api/FoiDeGen.ts
@@ -1,31 +1,30 @@
 const frases: string[] = [
-    "Foi de base",
-    "Foi de arrasta pra cima",
-    "Foi de hexa 2022",
-    "Foi de comes e bebes",
-    "Foi de deitado eternamente em berço esplêndido",
-    "Foi de se vira nos 30",
-    "Foi de Wakanda Forever na horizontal",
-    "Foi de link na bio",
-    "Foi de camisa de saudades",
-    "Foi de foto em preto e branco",
-    "Foi de Jacksons Five",
-    "Foi de F no chat",
-    "Foi de Drake e Josh",
-    "Foi pra terra jogar de pé junto",
-    "Foi jogar no Vasco",
-    "Foi de Rainha Elizabeth",
-    "Foi de Encontro com Fátima Bernardes",
-    "Foi de Bom dia e Cia",
-    "Foi pro show do Avicii",
-    "Foi de lojas americanas",
-    "Foi de Pelé Arantes do Nascimento",
-    "Foi de maradona"
-]
+	'Foi de base',
+	'Foi de arrasta pra cima',
+	'Foi de hexa 2022',
+	'Foi de comes e bebes',
+	'Foi de deitado eternamente em berço esplêndido',
+	'Foi de se vira nos 30',
+	'Foi de Wakanda Forever na horizontal',
+	'Foi de link na bio',
+	'Foi de camisa de saudades',
+	'Foi de foto em preto e branco',
+	'Foi de Jacksons Five',
+	'Foi de F no chat',
+	'Foi de Drake e Josh',
+	'Foi pra terra jogar de pé junto',
+	'Foi jogar no Vasco',
+	'Foi de Rainha Elizabeth',
+	'Foi de Encontro com Fátima Bernardes',
+	'Foi de Bom dia e Cia',
+	'Foi pro show do Avicii',
+	'Foi de lojas americanas',
+	'Foi de Pelé Arantes do Nascimento',
+	'Foi de maradona'
+];
 
 export function gerarFrase() {
-    const i = Math.floor(Math.random() * frases.length)
-    const frase = frases[i]
-    console.log(frase)
-    return frase
+	const i = Math.floor(Math.random() * frases.length);
+	const frase = frases[i];
+	return frase;
 }
--- api/main.ts
+++ api/main.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import { gerarFrase } from './FoiDeGen'
+import express from 'express';
+import cors from 'cors';
+import { gerarFrase } from './FoiDeGen';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    const frase = gerarFrase()
-    return res.json({ frase: frase })
-})
+	const frase = gerarFrase();
+	return res.json({ frase: frase });
+});
 
-app.listen(3333, () => console.log('Online em http://localhost:3333'))
+app.listen(3333, () => console.log('Online em http://localhost:3333'));
